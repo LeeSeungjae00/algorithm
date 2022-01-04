e6f54af283aca2564c4545df60b79031cf7087f0
LeeSeungjae00 <seungjae266789@gmail.com>
2022-01-04 22:20:14 +0900

    1152 done

---
+++ 1152.js
@@ -0,0 +1,20 @@
+const { SourceMap } = require("module");
+var readline = require("readline");
+var r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+r.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+  main(input);
+  process.exit();
+});
+
+function main(input){
+    if(input[0] === " ") {console.log(0); return;}
+    console.log(input[0].trim().split(' ').length);
+}
