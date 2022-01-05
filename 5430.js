58a96575ac53435a167f8f60b20073a132393864
LeeSeungjae00 <seungjae266789@gmail.com>
2022-01-05 11:13:29 +0900

    2908 Done

---
+++ 5430.js
@@ -0,0 +1,18 @@
+var readline = require("readline");
+var r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+r.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    main(input);
+    process.exit();
+});
+
+function main(input) {
+    const [T, p, ]
+}
