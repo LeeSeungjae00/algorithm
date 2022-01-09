39ae95ddd456df284d262972603e2919dd73a39b
LeeSeungjae00 <seungjae266789@gmail.com>
2022-01-09 21:23:19 +0900

    5622 done

--- 5622.js
+++ 5622.js
@@ -13,11 +13,42 @@ r.on("line", function (line) {
     process.exit();
 });
 
+let dialMap = {
+    'A' : 3,
+    'B' : 3,
+    'C' : 3,
+
+    'D' : 4,
+    'E' : 4,
+    'F' : 4,
+
+    'G' : 5,
+    'H' : 5,
+    'I' : 5,
+    'J' : 6,
+    'K' : 6,
+    'L' : 6,
+    'M' : 7,
+    'N' : 7,
+    'O' : 7,
+    'P' : 8,
+    'Q' : 8,
+    'R' : 8,
+    'S' : 8,
+    'T' : 9,
+    'U' : 9,
+    'V' : 9,
+    'W' : 10,
+    'X' : 10,
+    'Y' : 10,
+    'Z' : 10,
+}
+
 function main(input){
     const strArr = input[0].split('');
     let result = 0;
     strArr.forEach(chr => {
-        result += chr.charCodeAt()-65 + 2;
+        result += dialMap[chr];
     });
 
     console.log(result);
