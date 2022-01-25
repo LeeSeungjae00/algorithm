6f5fabffe96cfb2d9d24ea8f852d726254e11939
LeeSeungjae00 <seungjae266789@gmail.com>
2022-01-25 16:44:10 +0900

    3035 done

---
+++ 3053.js
@@ -0,0 +1,18 @@
+var readline = require("readline");
+var r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+r.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    main(input);
+    process.exit();
+});
+
+function main(input) {
+    console.log(Math.PI * input[0] * input[0]  + "\n" + input[0] * 2 * input[0]);
+}
