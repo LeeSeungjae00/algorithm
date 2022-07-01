28c7b02253b29fd6a70c5bdd0860af64c343dab3
LeeSeungjae00 <seungjae266789@gmail.com>
2022-07-02 01:35:10 +0900

    1074 done

--- 1074.js
+++ 1074.js
@@ -13,69 +13,35 @@ r.on("line", function (line) {
     process.exit();
 });
 
+let result = 0
 
-function madeEmptyArray(x, y) {
-    let arr = new Array(x).fill(0);
+function Z(length, x, y) {
+    const flag = length / 2;
 
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(y).fill(0);
+    console.log(result)
+
+    if (length === 1) return;
+
+    if (x < flag && y < flag) {
+        Z(flag, x, y)
     }
-
-    return arr
+    else if (x >= flag && y < flag) {
+        result += (flag * flag);
+        Z(flag, x - flag, y)
+    }
+    else if (x < flag && y >= flag) {
+        result += (flag * flag) * 2
+        Z(flag, x, y - flag)
+    }else{
+        result += (flag * flag) * 3
+        Z(flag, x - flag, y - flag)
+    }
 }
 
 function main(input) {
-    const [T, ...testCases] = input;
-    const result = []
+    const [N, r, c] = input[0].split(" ").map(Number);
 
-    for (let i = 0; i < Number(T); i++) {
-        const [M, N, K] = testCases[0].split(" ").map(Number)
-        const ground = madeEmptyArray(M, N);
+    Z(2 ** N, c, r);
 
-        for (let j = 1; j <= K; j++) {
-            const [x, y] = testCases[j].split(" ").map(Number)
-            ground[x][y] = 1;
-        }
-
-
-        const visited = madeEmptyArray(M, N);
-        let count = 0;
-        const queue = [];
-
-        ground.forEach((arr, xIndex) => {
-            arr.forEach((val, yIndex) => {
-                if (!visited[xIndex][yIndex] && val === 1) {
-                    queue.push([xIndex, yIndex]);
-
-                    while (queue.length !== 0) {
-                        const nowNode = queue.pop();
-                        visited[nowNode[0]][nowNode[1]] = 1;
-
-                        if (nowNode[1] + 1 < arr.length && !visited[nowNode[0]][nowNode[1] + 1] && ground[nowNode[0]][nowNode[1] + 1] === 1) {
-                            queue.push([nowNode[0], nowNode[1] + 1])
-                        }
-                        if (nowNode[0] + 1 < ground.length && !visited[nowNode[0] + 1][nowNode[1]] && ground[nowNode[0] + 1][nowNode[1]] === 1) {
-                            queue.push([nowNode[0] + 1, nowNode[1]])
-                        }
-                        if (nowNode[1] - 1 >= 0 && !visited[nowNode[0]][nowNode[1] - 1] && ground[nowNode[0]][nowNode[1] - 1] === 1) {
-                            queue.push([nowNode[0], nowNode[1] - 1])
-                        }
-                        if (nowNode[0] - 1 >= 0 && !visited[nowNode[0] - 1][nowNode[1]] && ground[nowNode[0] - 1][nowNode[1]] === 1) {
-                            queue.push([nowNode[0] - 1, nowNode[1]])
-                        }
-                    }
-
-                    ++count;
-                }
-            })
-        })
-
-        result.push(count)
-
-
-        testCases.splice(0, K + 1);
-
-    }
-
-    console.log(result.join('\n'))
+    console.log(result)
 }
