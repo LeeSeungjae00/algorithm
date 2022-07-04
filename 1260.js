572a6bf2794d46c88304e8fd54d2b853926614f5
LeeSeungjae00 <seungjae266789@gmail.com>
2022-07-04 22:37:53 +0900

    1260 done

--- 1260.js
+++ 1260.js
@@ -23,10 +23,57 @@ function main(input) {
         graph[to].push(from);
     });
 
-    console.log(edges, graph)
+    graph.forEach((arr) => {
+        arr.sort((a, b) => b - a)
+    })
+
+    dfs(V, graph)
+    bfs(V, graph)
 
 }
 
+function dfs(start, graph) {
+    graph.forEach((arr) => {
+        arr.sort((a, b) => b - a)
+    })
+    const visited = [...Array(graph.length)].fill(0);
+    const queue = [];
+    const result = [];
+    queue.push(start);
 
+    while (queue.length) {
+        let node = queue.pop();
+        if (!visited[node]) {
+            visited[node] = 1;
+            result.push(node);
+            graph[node].forEach((val) => {
+                queue.push(val);
+            })
+        }
 
+    }
+    console.log(result.join(' '))
+}
 
+function bfs(start, graph) {
+    graph.forEach((arr) => {
+        arr.sort((a, b) => a - b)
+    })
+    const visited = [...Array(graph.length)].fill(0);
+    const queue = [];
+    const result = [];
+    queue.push(start);
+
+    while (queue.length) {
+        let node = queue.shift();
+        if (!visited[node]) {
+            visited[node] = 1;
+            result.push(node);
+            graph[node].forEach((val) => {
+                queue.push(val);
+            })
+        }
+
+    }
+    console.log(result.join(' '))
+}
