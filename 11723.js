40d31911b000a0a8c9431b52baf44b39d2add91b
LeeSeungjae00 <seungjae266789@gmail.com>
2022-07-13 11:38:29 +0900

    11723 done

---
+++ 11723.js
@@ -0,0 +1,52 @@
+var readline = require("readline");
+var r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+r.on("line", function (line) {
+    input.push(line);
+}).on("close", function () {
+    main(input);
+    process.exit();
+});
+
+
+function main(input) {
+    let arr = new Array(21).fill(false);
+
+    const M = input.shift();
+
+
+    input.forEach(inp => {
+        const [cmd, val] = inp.split(" ");
+        switch (cmd) {
+            case "add":
+                arr[+val] = true
+                break;
+            case "remove":
+                arr[+val] = false
+                break;
+            case "check":
+                if(arr[+val]){
+                    console.log(1)
+                }else{
+                    console.log(0)
+                }
+                break;
+            case "toggle":
+                arr[+val] = !arr[+val]
+                break;
+            case "all":
+                arr.fill(true);
+                break;
+            case "empty":
+                arr.fill(false);
+                break;
+            default:
+                break;
+        }
+    });
+}
