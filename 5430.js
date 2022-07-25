007c2d33546b29f5063c900770d655eb6a76c7c2
LeeSeungjae00 <seungjae266789@gmail.com>
2022-07-25 14:55:50 +0900

    5430 done

--- 5430.js
+++ 5430.js
@@ -14,5 +14,35 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-    const [T, p, ]
+    const T = +input.shift();
+    const result = []
+
+    const AC = (p, n, arr) => {
+        let flag = true
+        let cnt = 0
+
+        for (let i = 0; i < p.length; i++) {
+            let cmd = p[i];
+            if (cmd === 'R') {
+                flag = !flag
+            } else {
+                if(arr.length - cnt <= 0) return "error"
+                flag ? cnt++ : arr.pop()
+            }
+        }
+
+        arr.splice(0,cnt)
+
+        return `[${flag ? arr.toString() : arr.reverse().toString()}]`
+    }
+
+    for (let i = 0; i < input.length; i+= 3) {
+        const p = input[i].split('')
+        const n = +input[i+1]
+        const arr = JSON.parse(input[i + 2])
+
+        result.push(AC(p, n, arr))
+    }
+
+    console.log(result.join('\n'))
 }
