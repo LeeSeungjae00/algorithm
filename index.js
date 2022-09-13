6bfb7beb08c90fc483328d643d9c2e457253ca64
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-14 00:24:01 +0900

    트리 순회 미완료

--- index.js
+++ index.js
@@ -15,23 +15,36 @@ r.on("line", function (line) {
 
 function main(input) {
     const n = Number(input.shift())
-    const inOrder = input.shift().split(' ')
+    let indexMap = {}
+    const inOrder = input.shift().split(' ').map((val, i) => { 
+        indexMap[val] = i
+        return val
+    })
     const postOrder = input.shift().split(' ')
     const graph = [];
 
-    function tree(start, end) {
+    function tree(start, len) {
+        if(start + len < 0) return
+        if(len < 0) return
+        if(len === 0){
+            graph.push(postOrder[start + len])
+            return
+        } 
+        if(len === 1){
+            graph.push(postOrder[start + 1])
+            graph.push(postOrder[start ])
+            return
+        } 
+        let startIndex = indexMap[postOrder[start]]
+        if(!startIndex) startIndex = 0
+        let index = indexMap[postOrder[start + len]]
+        graph.push(postOrder[start + len])
 
-        graph.push(postOrder[start + end])
-        if (end <= 0) return
-        const inRoot = inOrder.indexOf(postOrder[start + end])
-        tree(start, inRoot - 1 - start)
-        tree(inRoot - start, end - inRoot - start - 1)
+        tree(start, index - startIndex - 1);
+        tree(start + index - startIndex, len + startIndex - index - 1)
     }
 
-    console.log(tree(0, n - 1), graph)
+    tree(0, n- 1)
+    console.log(graph.join(' '))
 
 }
-
-main(`6
-5 2 4 1 6 3
-5 4 2 6 3 1`.split('\n'))
