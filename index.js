d1057b97d7f31ec345cdde60d734d01c2a517707
LeeSeungjae00 <seungjae266789@gmail.com>
2022-09-20 22:03:15 +0900

    merge

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ r.on("line", function (line) {
 });
 
 function main(input) {
-<<<<<<< HEAD
     const [N, M] = input[0].split(' ').map(Number)
     const result = []
 
@@ -39,40 +38,3 @@ function main(input) {
 
     console.log(result.join('\n'))
 }
-=======
-    const T = +input.shift()
-    let index = 0
-
-    function DP(dp, arr1, arr2, N){
-        dp[0][0] = arr1[0]
-        dp[1][0] = arr2[0]
-        dp[0][1] = arr2[0] + arr1[1]
-        dp[1][1] = arr2[1] + arr1[0]
-
-        for (let i = 2; i < N; i++) {
-            //-2 스티커중 큰 값
-            const big = dp[1][i - 2] > dp[0][i - 2] ? dp[1][i - 2] : dp[0][i - 2]
-            dp[0][i] = (big + arr1[i]) > (dp[1][i - 1] + arr1[i]) ? big + arr1[i] : (dp[1][i - 1] + arr1[i])
-            dp[1][i] = (big + arr2[i]) > (dp[0][i - 1] + arr2[i]) ? big + arr2[i] : (dp[0][i - 1] + arr2[i])
-        }
-
-        return (dp[0][N - 1] > dp[1][N - 1]) ? dp[0][N - 1] : dp[1][N - 1]
-    }
-
-    for (let i = 0; i < T; i++) {
-        const N = +input[index++]
-        const dp = [new Array(N),new Array(N)]
-        const arr1 = input[index++].split(' ').map(Number)
-        const arr2 = input[index++].split(' ').map(Number)
-
-        if(N === 1){
-            console.log(arr1[0] > arr2[0] ? arr1[0] : arr2[0])
-            continue;
-        }
-
-        console.log(DP(dp, arr1, arr2, N))
-    }
-}
-
-
->>>>>>> b100e2d89f75b2a782014bb7e21e933553da2621
